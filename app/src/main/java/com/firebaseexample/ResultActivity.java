8fcd36d4c4289a885704f5ee268b125ca1c498de
BenMbarek <houssem.ben.mbarek@gmail.com>
2017-04-07 16:26:46 +0100

    init

---
+++ app/src/main/java/com/firebaseexample/ResultActivity.java
@@ -0,0 +1,21 @@
+package com.firebaseexample;
+
+/**
+ * Created by houss on 07/04/17.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+    private TextView textView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("Welcome to the Result Activity");
+    }
+}
